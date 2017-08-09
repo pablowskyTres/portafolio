a3d3c12be4db0c4ebc55f257ce19c090fa1e2d14
pablowsky <p.badef@gmail.com>
2017-08-09 09:39:22 -0400

    Se agrega index de prueba 2

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php  
+$html =<<<HTML
+	<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Portafolio Pablo Bade</title>
+	</head>
+	<body>
+		<div>
+			<h1>Hola Mundo!</h1>
+		</div>
+	</body>
+	</html>
+HTML;
+echo $html;
+?>
